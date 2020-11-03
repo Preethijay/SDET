6818297da5fb089cbba1a57469ebdf1e3cbe10b4
Preethijay <73516606+Preethijay@users.noreply.github.com>
2020-11-03 19:20:51 +0530

    Add files via upload

---
+++ Python/Activity 1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name")
+age = int(input("Enter your age"))
+year = str((2020 - age) + 100)
+
+print(name + " will be 100 years old in the year " +year) 
+
---
+++ Python/Activity 2.py
@@ -0,0 +1,8 @@
+
+Number = int(input("Enter a number : " ))
+mod = Number % 2
+if (mod > 0) :
+ print("Number is ODD")
+
+else:
+  print("Number is EVEN")
