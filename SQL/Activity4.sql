12145f2fce25bf5a776ff723785c9dd8cf3d37ac
Preethijay <73516606+Preethijay@users.noreply.github.com>
2020-11-03 15:07:48 +0530

    Add files via upload

---
+++ SQL/Activity4.sql
@@ -0,0 +1,7 @@
+Alter table salesman add grade int;
+
+update salesman set grade=100;
+
+Select * from salesman;
+
+update salesman set grade=200 where city='Rome';
---
+++ SQL/Activity5.sql
@@ -0,0 +1,7 @@
+Select * from salesman;
+
+update salesman set grade=200 where city='Rome';
+
+update salesman set grade=300 where name='James Hoog';
+
+update salesman set name ='Pierre' where name ='McLyon';
