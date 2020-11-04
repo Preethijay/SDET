1f2b6a066dd5f804062010ba21b0faffc40d9947
Preethijay <73516606+Preethijay@users.noreply.github.com>
2020-11-04 17:19:16 +0530

    Add files via upload

---
+++ SQL/Acitivty10.sql
@@ -0,0 +1,16 @@
+SELECT * FROM orders
+WHERE salesman_id=(SELECT Distinct salesman_id FROM orders WHERE customer_id=3007);
+
+select * from orders;
+select * from salesman;
+
+select * from orders
+where salesman_id = (select salesman_id from salesman where city = 'New York');
+
+SELECT grade, COUNT(*) FROM customers
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders
+WHERE salesman_id IN( SELECT salesman_id FROM salesman
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
