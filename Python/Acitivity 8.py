3c9fe319a8a03200641366efdda8ede1bf86e8df
Preethijay <73516606+Preethijay@users.noreply.github.com>
2020-11-10 19:48:36 +0530

    Add files via upload

---
+++ Python/Acitivity 8.py
@@ -0,0 +1,13 @@
+numlist = [10,21,23,43,10,1]
+print("Given list of numbers ", numlist)
+
+firstnum= numlist[0]
+
+lastnum= numlist[-1]
+
+
+if(firstnum== lastnum):
+   print(True)
+
+else:
+    print(False)
