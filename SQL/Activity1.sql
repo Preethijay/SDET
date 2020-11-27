e2686db61b006f74e957920daa8f54308bd008a9
Preethijay <73516606+Preethijay@users.noreply.github.com>
2020-11-27 13:04:17 +0530

    Create Activity1.sql

---
+++ SQL/Activity1.sql
@@ -0,0 +1,12 @@
+create database sdet4_preethij;
+
+use sdet4_preethij;
+
+CREATE TABLE salesman (
+    salesman_id int primary key,
+    name varchar(32),
+    city varchar(32),
+    commission int
+);
+
+describe salesman; 
